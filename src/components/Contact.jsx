e5b967be4ad48e4339f30fe36a00696ac8c1ce93
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2025-01-11 21:15:12 -0600

    Update contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Contact() {
   return (
-      <div className="h-40 flex justify-center self-stretch">
-        <a href="www.linkedin.com/in/jerry-hudgins-jr">LinkdeIn</a>
-        <a href="https://github.com/jerryh-jr">Git Hub</a>
+      <div className="h-40 flex justify-center gap-4">
+        <a href="www.linkedin.com/in/jerry-hudgins-jr">LinkedIn</a>
+        <a href="https://github.com/jerryh-jr">GitHub</a>
         <p>Email: jerry@jerry-codes.com</p>
       </div>
   )
