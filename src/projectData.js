e663c92da74b89c5507677a8203ee95a2e9fb556
Jerry <47369872+jerryh-jr@users.noreply.github.com>
2025-01-11 20:51:43 -0600

    Readd broken images and refactor to use require

--- src/projectData.js
+++ src/projectData.js
@@ -1,33 +1,31 @@
-import cuttle from './assets/images/cuttle.png';
-
 const projects = [
     {
         title: "Cuttle - Open Source Contributor",
-        img: cuttle,
+        img: require("./assets/images/cuttle.png"),
         link: 'https://github.com/cuttle-cards/cuttle/commits?author=jerryh-jr',
         linkText: 'Cuttle Commits'
     },
     {
         title: "Add to Cart",
-        img: "./assets/addToCart.png",
+        img: require("./assets/images/addToCart.png"),
         link: 'https://add-to-cart-jerry.netlify.app/',
         linkText: 'Shopping list app.'
     },
     {
         title: 'Champion Endorsements',
-        img: "./assets/champions.png",
+        img: require("./assets/images/champions.png"),
         link: 'https://jerry-champion.netlify.app/',
         linkText: 'Leave an endorsement and tell them why they are a champion!'
     },
     {
         title: 'Investigating Netflix Movies',
-        img: "./assets/netflixMovies.png",
+        img: require("./assets/images/netflixMovies.png"),
         link: 'https://www.datacamp.com/datalab/w/09fa35ce-7126-4379-82f0-bdddd26dde29/edit',
         linkText: 'Netflix Data'
     },
     {
         title: 'Exploring NYC Public Schools',
-        img: "./assets/nycData.png",
+        img: require("./assets/images/nycData.png"),
         link: 'https://www.datacamp.com/datalab/w/df8ca000-b6ef-4b8f-a74d-1284cd96f922/edit',
         linkText: 'School Data'
     },
